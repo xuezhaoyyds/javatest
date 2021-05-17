77a563439113c444fe66833e56232114fcf96fa2
hp <13201433786@163.com>
2021-05-17 23:49:18 +0800

    线程创建相关操作

--- MyRunnable.java
+++ MyRunnable.java
@@ -1,11 +1,11 @@
 package com.it.Test.demo1;
 
-public class MyRunnable extends Thread implements Runnable {
+public class MyRunnable implements Runnable {
 
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
-            System.out.println(getName() + i);
+            System.out.println(Thread.currentThread().getName() + i);
         }
     }
 }
